f6440d0ae61c4c9d4cacdd91e811ddcd359410e2
viaeou <13018056625@163.com>
2018-06-21 17:25:21 +0800

    add some revisions on blending.py to debug with random samples`:

--- model/blending.py
+++ model/blending.py
@@ -6,15 +6,16 @@ Created on Wed Jun 20 16:49:49 2018
 """
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
+import numpy as np
 
 class blending(object):
     
     def __init__(self, train_pred, train_y, val_pred, val_y, test_pred):
-        self.train_pred = train_pred
-        self.train_y = train_y
-        self.val_pred = val_pred
-        self.val_y = val_y
-        self.test_pred = test_pred
+        self.train_pred = train_pred.reshape(-1,1)
+        self.train_y = train_y.reshape(-1,1)
+        self.val_pred = val_pred.reshape(-1,1)
+        self.val_y = val_y.reshape(-1,1)
+        self.test_pred = test_pred.reshape(-1,1)
         
     def train(self):
         clf = LogisticRegression(
@@ -22,13 +23,16 @@ class blending(object):
             C = 10.0,
             class_weight = 'balanced',
             random_state = 14,
-            n_jobs = 72
+            n_jobs = 72,
+            solver= 'saga'
             )
-        clf.fit(self.train_pred, self.train_y)
-        lr_val_pred = clf.predict_proba(self.val_pred)
-        lr_test_pred = clf.predict_proba(self.test_pred)
-        print('Validate AUC score:', roc_auc_score(lr_val_pred, self.val_y))
+        clf.fit(self.train_pred, self.train_y.ravel())
+        lr_val_pred = clf.predict_proba(self.val_pred)[:, 1]
+        lr_test_pred = clf.predict_proba(self.test_pred)[:, 1]
+        print('Validate AUC score:', roc_auc_score(self.val_y.ravel(), lr_val_pred))
         return lr_test_pred
-    
+'''train_pred, train_y, val_pred, val_y, test_pred = np.random.random((7000,)), np.random.randint(0,2,size=(7000,)), np.random.random((3000,)), np.random.randint(0,2,size=(3000,)), np.random.random((3000,))
+bld = blending(train_pred, train_y, val_pred, val_y, test_pred)
+lr_test_pred = bld.train()'''
         
         
