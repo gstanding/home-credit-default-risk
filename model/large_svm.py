7751ac5131663810e738522e1e20bbe59cc003fe
viaeou <13018056625@163.com>
2018-07-10 08:48:26 +0800

    edit large_svm

--- model/large_svm.py
+++ model/large_svm.py
@@ -5,8 +5,11 @@ Created on Thu Jun 21 19:09:45 2018
 @author: gstanding
 """
 import numpy as np
+import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import normalize
 import random
 
 def pipeline(iteration,C,gamma,random_seed):
@@ -15,8 +18,8 @@ def pipeline(iteration,C,gamma,random_seed):
     clf = SVC(C=C,kernel='rbf',gamma=gamma,probability=True,cache_size=7000,class_weight='balanced',verbose=True,random_state=random_seed)
     clf.fit(x_train,y_train)
     
-    pred = clf.predict_proba(test_x)
-    val_res  = clf.predict_proba(val_x)
+    pred = clf.predict_proba(test_x_)[:, 1]
+    val_res  = clf.predict_proba(val_x_)[:, 1]
     return pred, val_res
 #    #predict test set
 #    pred = clf.predict_proba(test_x)
@@ -39,10 +42,24 @@ if __name__ == "__main__":
     random.shuffle(random_seed)
     random.shuffle(C)
     random.shuffle(gamma)
-    train_x, train_y = np.random.random((5000,10)), np.random.randint(0,2,size=(5000,))
-    val_x, val_y = np.random.random((50000,10)), np.random.randint(0,2,size=(50000,))
-    test_x = np.random.random((30000,10))
-
+    #train_x, train_y = np.random.random((5000,10)), np.random.randint(0,2,size=(5000,))
+    train_x, train_y = pd.read_csv('../data/train_.csv'), pd.read_csv('../data/y_.csv')
+    #print(train_x.shape)
+    #val_x, val_y = np.random.random((50000,10)), np.random.randint(0,2,size=(50000,))
+    val_x, val_y = pd.read_csv('../data/val_x_.csv'), pd.read_csv('../data/val_y_.csv')
+    #test_x = np.random.random((30000,10))
+    test_x = pd.read_csv('../data/test_.csv')
+    val_results = np.zeros((val_x.shape[0],))
+    test_results = np.zeros((test_x.shape[0],))
+    test_x_ = test_x[train_x.columns]
+    val_x_ = val_x[train_x.columns]
+    #print(val_results.shape)
     for i in range(50):
        pred, val_res = pipeline(i,C[i],gamma[i],random_seed[i])
-       print(pred.shape, val_res.shape)
+       val_results += val_res / 50
+       test_results += pred / 50
+       del pred
+       del val_res
+    test_x['TARGET'] = test_results
+    print('validation auc score:', roc_auc_score(val_y, val_results))
+    test_x[['SK_ID_CURR', 'TARGET']].to_csv('../data/large_svm_pred.csv')
